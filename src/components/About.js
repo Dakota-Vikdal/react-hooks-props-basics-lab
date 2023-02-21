f17973f2b0327859c0191c51b017b8bb94791330
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-21 18:12:43 -0500

    Learning props/ aka making the webpage dynamic

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links.js";
 
-function About() {
+function About(prop) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{prop.bio ? prop.bio : ""}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={prop.github} linkedin= {prop.linkedin}/>
     </div>
   );
 }
